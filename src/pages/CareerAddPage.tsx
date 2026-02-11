e3708cb1cd73ea1be5c717f0def8eb8e237bb12f
heewon-hong <ghdgmldnjs0521@gmail.com>
2026-02-12 01:22:02 +0900

    fix: remove unused getDisplayName

--- src/pages/CareerAddPage.tsx
+++ src/pages/CareerAddPage.tsx
@@ -3,7 +3,6 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { CircleCheck, FileText, Mic, X } from "lucide-react";
 
-import { getDisplayName } from "../utils/name";
 import { useUploadManager, type LinkItem } from "../hooks/useUploadManager";
 
 import { formatPeriod, getEndDateFromPeriod, getStartDateFromPeriod, validatePeriod } from "../utils/format";
